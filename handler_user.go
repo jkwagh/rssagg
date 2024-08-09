96423c39509bf737364f08eb6b4ef225b1ad527d
Jeremy <kwagh.jeremiah@gmail.com>
2024-08-09 01:32:40 -0400

    added authenticatio with API

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jkwagh/rssagg/internal/auth"
 	"github.com/jkwagh/rssagg/internal/database"
 )
 
@@ -37,5 +38,21 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	}
 
 	//replaced database user with our user created in models.go file to use version we control
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
+
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
--- models.go
+++ models.go
@@ -7,18 +7,22 @@ import (
 	"github.com/jkwagh/rssagg/internal/database"
 )
 
+//want to be able to configure the struct without touching the sqlc generated models.go file
+
 type User struct {
 	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time	`json:"created_at"`
-	UpdatedAt time.Time	`json:"updated_at"`
-	Name      string	`json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	APIKey    string    `json:"api_key"`
 }
 
 func databaseUserToUser(dbUser database.User) User {
 	return User{
-		ID: dbUser.ID,
+		ID:        dbUser.ID,
 		CreatedAt: dbUser.CreatedAt,
 		UpdatedAt: dbUser.UpdatedAt,
-		Name: dbUser.Name,
+		Name:      dbUser.Name,
+		APIKey:    dbUser.ApiKey,
 	}
 }
